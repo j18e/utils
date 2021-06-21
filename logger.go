179397365d721040b8b2725250f5453ea1140fd7
Jamie Wiebe <jamiewiebe@gmail.com>
2021-06-21 12:41:19 +0200

    remove test logger

--- logger.go
+++ logger.go
@@ -5,13 +5,3 @@ type Logger interface {
 	Info(...interface{})
 	Errorf(string, ...interface{})
 }
-
-// NewTestLogger does nothing and is only used for testing.
-func NewTestLogger() Logger {
-	return &testLogger{}
-}
-
-type testLogger struct{}
-
-func (l *testLogger) Info(...interface{})           {}
-func (l *testLogger) Errorf(string, ...interface{}) {}
